1dc8dd16367b70a1d1025ba19e7ae4cb951fc845
791272 <36864616+791272@users.noreply.github.com>
2019-01-14 08:50:11 -0800

    Add files via upload

--- Labs/Lab 0109/script.js
+++ Labs/Lab 0109/script.js
@@ -14,17 +14,22 @@ function init(){
 function getJSON(){
   var array = [];
   $.getJSON("data.json", function(json) {
-    for (var i = 0; i < json.meteors.length; i++){
-      array[i] = json.meteors[i].mass;
+  var doNotList;
+    for (var k = 0; k < json.meteors.length-950; k++){
+      if(){
+       doNotList++;
+      }else{
+        array[k] = json.meteors[k].mass;
+      }
     }
-    
-  /* bubbleSort
+
+ //bubbleSort
     for (var i = array.length-1; i > 0; i--){
       for(var j = 0; j < i; j++){
-        if (array[j].length > array[j+1].length){
-          var temp = array[j].length;
-            array[j].length = array[j+1].length;
-            array[j+1].length = temp;
+          if (Number(array[j]) > Number(array[j+1])){
+            var temp = array[j];
+            array[j] = array[j+1];
+            array[j+1] = temp;
           }
         }
       }
@@ -54,6 +59,29 @@ function getJSON(){
       }
     }
   */
+
+  /* mySort
+    var frontCount = 0;
+    var backCount = array.length-1;
+    for(var i = 0; i < array.length/2; i++){
+      for(var j = frontCount; j < backCount; j++){
+        if(array[j].length > array[j+1].length){
+          var temp = array[j];
+          array[j] = array[j+1];
+          array[j+1] = temp;
+        }
+      }
+      backCount--;
+      for(var k = backCount; k > frontCount; k--){
+        if(array[k].length < array[k-1].length){
+          var temp = array[k];
+          array[k] = array[k-1];
+          array[k-1] = temp;
+        }
+      }
+      frontCount++;
+    }
+    */
 console.log(array);
 });
 }
